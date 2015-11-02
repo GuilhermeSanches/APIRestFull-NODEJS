cb0d190cf6223d5dee08b590d27f36b0614cb1c3
Guilherme Sanches Pereira <guilherme_1993pa@hotmail.com>
2015-11-02 21:49:20 -0200

    Correção Arquivos

--- bin/www.js
+++ bin/www.js
@@ -7,23 +7,12 @@ var app = require('../app');
 
 
 
-
-/**
- *  Define the sample application.
- */
 var SampleApp = function() {
 
-    //  Scope.
+ 
     var self = this;
 
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
@@ -37,101 +26,10 @@ var SampleApp = function() {
         };
     };
 
-
-    /**
-     *  Populate the cache.
-     */
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-  /*
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('./angular/index.html'));
-        };
-        self.routes['/views/menu-top.html'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('./angular/views/menu-top.html'));    
-        };
-            
-        self.routes['/views/read.html'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('./angular/views/read.html'));  
-        };
-            
-        self.routes['http://localhost/angular/js/app.js'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/javascript');
-            res.send(self.cache_get('js/app.js') );
-        };
-        self.routes['/js/controller.js'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/script');
-            res.send(self.cache_get('./angular/js/controller.js') );
-        };
-        self.routes['/js/service.js'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/javascript');
-            res.send(self.cache_get('./angular/js/service.js') );
-        };
-        self.routes['/bower_components/angular/angular.min.js'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/javascript');
-            res.send(self.cache_get('/angular/bower_components/angular/angular.min.js') );
-        };
-        self.routes['/css/bootstrap.min.css'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/css');
-            res.send(self.cache_get('./angular/css/bootstrap.min.css') );
-        };
-        self.routes['/bower_components/angular-route/angular-route.min.js'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/javascript');
-            res.send(self.cache_get('./angular/bower_components/angular-route/angular-route.min.js') );
-        };
-    };
-
-*/
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
     self.initializeServer = function() {
         //self.createRoutes();
-        self.app2 = express();
-
-        
+        self.app2 = express();        
     };
-
 
     /**
      *  Initializes the sample application.
@@ -139,9 +37,7 @@ var SampleApp = function() {
     self.initialize = function() {
         self.setupVariables();
       
-        
-     
-
+             
         // Create the express server and routes.
         self.initializeServer();
     };
@@ -158,8 +54,7 @@ var SampleApp = function() {
         });
     };
 
-};   /*  Sample Application.  */
-
+}; 
 
 
 /**
